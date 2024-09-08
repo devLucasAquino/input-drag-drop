cfecc9d70e911c248b1121acb60f95e4cfb527d3
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-08 18:30:40 -0300

    feat: created basic structure and FileInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { FileInput } from "./components/FileInput";
+
 function App() {
+
+  const onSubmit = () => {};
 
   return (
     <div>
-      <p className="">Hello World</p>
+      <h1 className="font-bold text-center text-white my-16 text-4xl">
+        Input files Drag/Drop
+      </h1>
+      <form onSubmit={onSubmit} className="w-full h-72 flex justify-center items-center">
+        <FileInput />
+      </form>
     </div>
   )
 }
---
+++ src/components/FileInput.tsx
@@ -0,0 +1,7 @@
+export const FileInput = () => {
+    return(
+        <div>
+            <h2>trc</h2>
+        </div>
+    )
+}
